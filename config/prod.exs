ae58a11fdb190362aa06affcc64bc7aff8530f5f
Navin Peiris <navinpeiris@gmail.com>
2016-10-12 14:59:25 +0530

    Changing site from https to http

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenix_diff, PhoenixDiff.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "www.phoenixdiff.org", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "www.phoenixdiff.org", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
