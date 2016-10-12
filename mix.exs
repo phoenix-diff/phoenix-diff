2ae44eb650b7db57502fc0a269b075cecceed5da
Navin Peiris <navinpeiris@gmail.com>
2016-10-12 17:01:44 +0530

    Adding credo for static analysis

--- mix.exs
+++ mix.exs
@@ -36,6 +36,8 @@ defmodule PhoenixDiff.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
+
+      {:credo, "~> 0.4", only: [:dev, :test]}
     ]
   end
 end
