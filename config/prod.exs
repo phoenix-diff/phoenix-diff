d503e6c8538a3b9256c59a351fc9edbad3919ba3
Navin Peiris <navinpeiris@gmail.com>
2016-11-08 20:57:00 +0530

    Force connections to be over SSL

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenix_diff, PhoenixDiff.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "www.phoenixdiff.org", port: 80],
+  url: [scheme: "https", host: "www.phoenixdiff.org", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
