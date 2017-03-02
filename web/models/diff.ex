e2e461596a9ce97b76a8753ec11ab6e2496414c2
Navin Peiris <navinpeiris@gmail.com>
2017-03-02 13:37:22 +0530

    Generating and storing diffs between all the different versions
    This is because the diffs generated on the server arent very good at handling file moves, which are a huge part of 1.3.0-rc.0

--- web/models/diff.ex
+++ web/models/diff.ex
@@ -1,9 +1,28 @@
 defmodule PhoenixDiff.Diff do
   @sample_app_path "data/sample-app"
+  @diffs_path "data/diffs"
 
   def available_versions, do: @sample_app_path |> File.ls! |> Enum.sort
 
+  def store_diffs do
+    versions = available_versions()
+    for from_version <- versions do
+      for to_version <- versions do
+        File.write! diff_file_path(from_version, to_version), generate(from_version, to_version)
+      end
+    end
+  end
+
   def get(from_version, to_version) do
+    case File.read(diff_file_path(from_version, to_version)) do
+      {:ok, content} -> content
+      _ -> ""
+    end
+  end
+
+  defp diff_file_path(from_version, to_version), do: "#{@diffs_path}/#{from_version}--#{to_version}.diff"
+
+  defp generate(from_version, to_version) do
     from_path = "#{@sample_app_path}/#{from_version}"
     to_path = "#{@sample_app_path}/#{to_version}"
 
