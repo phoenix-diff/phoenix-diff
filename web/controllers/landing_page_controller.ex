7eaf13743a043ebdfd5ee933427a4902d8564d8c
Chulki Lee <chulki.lee@gmail.com>
2017-03-09 09:36:13 -0800

    Use query parameter on landing page

--- web/controllers/landing_page_controller.ex
+++ web/controllers/landing_page_controller.ex
@@ -3,13 +3,12 @@ defmodule PhoenixDiff.LandingPageController do
 
   alias PhoenixDiff.Diff
 
-  def index(conn, _params) do
+  def index(conn, params) do
     available_versions = Diff.available_versions
-
     conn
     |> assign(:available_versions, available_versions)
-    |> assign(:target_version, available_versions |> List.last)
-    |> assign(:source_version, available_versions |> Enum.at(-3))
+    |> assign(:target_version, Map.get(params, "target_version", available_versions |> List.last))
+    |> assign(:source_version, Map.get(params, "source_version", available_versions |> Enum.at(-3)))
     |> render("index.html")
   end
 end
