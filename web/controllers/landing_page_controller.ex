cd177f130e75c9b531c2c380f611d1eb67e7f1e3
Navin Peiris <navinpeiris@gmail.com>
2017-03-15 11:03:07 +0530

    Generating diffs for 1.3.0-rc.1

--- web/controllers/landing_page_controller.ex
+++ web/controllers/landing_page_controller.ex
@@ -9,7 +9,7 @@ defmodule PhoenixDiff.LandingPageController do
     conn
     |> assign(:available_versions, available_versions)
     |> assign(:latest_version, available_versions |> List.last)
-    |> assign(:previous_version, available_versions |> Enum.at(-2))
+    |> assign(:previous_version, available_versions |> Enum.at(-3))
     |> render("index.html")
   end
 end
