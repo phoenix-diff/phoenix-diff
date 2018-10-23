aad813b25d439f9bd8e1b580353cf4521e4a9bb4
Navin Peiris <navinpeiris@gmail.com>
2018-10-23 10:55:18 +0530

    Update endpoint to serve favicons

--- lib/phx_diff_web/endpoint.ex
+++ lib/phx_diff_web/endpoint.ex
@@ -13,7 +13,17 @@ defmodule PhxDiffWeb.Endpoint do
     at: "/",
     from: :phx_diff,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(
+      css
+      fonts
+      images
+      js
+      favicon.ico
+      favicon-16x16.png
+      favicon-32x32.png
+      favicon-96x96.png
+      robots.txt
+    )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
