5a4613c88c42d1dd3dd01f6dba56c18b70bda291
Navin Peiris <navinpeiris@gmail.com>
2018-10-23 11:34:10 +0530

    Basic content for landing page

--- lib/phx_diff_web/controllers/page_controller.ex
+++ lib/phx_diff_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule PhxDiffWeb.PageController do
   use PhxDiffWeb, :controller
 
+  alias PhxDiff.Diffs
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> assign(:all_versions, Diffs.all_versions())
+    |> assign(:source_version, Diffs.previous_release_version())
+    |> assign(:target_version, Diffs.latest_version())
+    |> render("index.html")
   end
 end
