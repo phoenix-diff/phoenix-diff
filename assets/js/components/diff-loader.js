13091cea6d94e6803dc8030a04f5afcbedebc749
Navin Peiris <navinpeiris@gmail.com>
2018-10-25 10:14:46 +0530

    Fetch and show diff

--- assets/js/components/diff-loader.js
+++ assets/js/components/diff-loader.js
@@ -21,11 +21,50 @@ export default {
     this.sourceVersion = this.source;
     this.targetVersion = this.target;
 
+    this.diffResultsContainer = jQuery('#diff-results-container');
+
     this.loadDiff();
   },
   methods: {
     loadDiff() {
       this.loading = true;
+      this.noChanges = false;
+
+      this._clearDiffContainer();
+
+      this._fetchDiff()
+        .then(diff => {
+          this.loading = false;
+
+          if (diff) {
+            this._showDiff(diff);
+          } else {
+            this.noChanges = true;
+          }
+        });
+    },
+    _clearDiffContainer() {
+      this.diffResultsContainer.empty();
+    },
+    _fetchDiff() {
+      return axios
+              .get(`/diffs?source=${this.sourceVersion}&target=${this.targetVersion}`)
+              .then(response => response.data);
+    },
+    _showDiff(diff) {
+      var diff2htmlUi = new Diff2HtmlUI({diff: diff});
+
+      diff2htmlUi.draw(
+        this.diffResultsContainer,
+        {
+          inputFormat: 'diff',
+          outputFormat: 'line-by-line',
+          showFiles: true,
+          matching: 'lines'
+        }
+      );
+
+      diff2htmlUi.fileListCloseable(this.diffResultsContainer, false);
     }
   }
 }
