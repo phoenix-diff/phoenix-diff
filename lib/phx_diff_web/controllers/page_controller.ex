2bfb03f821b9540ee5e3810bed173be3f33483cc
Navin Peiris <navinpeiris@gmail.com>
2018-10-25 11:54:56 +0530

    Use the source and target versions from the URL if available

--- lib/phx_diff_web/controllers/page_controller.ex
+++ lib/phx_diff_web/controllers/page_controller.ex
@@ -3,11 +3,11 @@ defmodule PhxDiffWeb.PageController do
 
   alias PhxDiff.Diffs
 
-  def index(conn, _params) do
+  def index(conn, params) do
     conn
     |> assign(:all_versions, Diffs.all_versions())
-    |> assign(:source_version, Diffs.previous_release_version())
-    |> assign(:target_version, Diffs.latest_version())
+    |> assign(:source_version, Map.get(params, "source", Diffs.previous_release_version()))
+    |> assign(:target_version, Map.get(params, "target", Diffs.latest_version()))
     |> render("index.html")
   end
 end
