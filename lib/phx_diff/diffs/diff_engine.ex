ac73f32035a22559f888581975b7bff0f6bc93ba
Aaron Renner <aaron.renner@gmail.com>
2020-06-26 14:20:47 -0600

    Simplify DiffEngine
    DiffEngine can now focus on diffing instead of calling out to app repo to fetch
    versions. This moves the cross module calls to PhxDiff.Diffs.

--- lib/phx_diff/diffs/diff_engine.ex
+++ lib/phx_diff/diffs/diff_engine.ex
@@ -1,32 +1,10 @@
 defmodule PhxDiff.Diffs.DiffEngine do
   @moduledoc false
 
-  alias PhxDiff.Diffs.{
-    AppRepo,
-    AppSpecification,
-    Config
-  }
-
   @type diff :: PhxDiff.Diffs.diff()
 
-  @spec get_diff(Config.t(), AppSpecification.t(), AppSpecification.t()) ::
-          {:ok, diff} | {:error, :invalid_versions}
-  def get_diff(
-        %Config{} = config,
-        %AppSpecification{} = source_spec,
-        %AppSpecification{} = target_spec
-      ) do
-    with {:ok, source_path} <- AppRepo.fetch_app_path(config, source_spec),
-         {:ok, target_path} <- AppRepo.fetch_app_path(config, target_spec) do
-      diff = compute_diff!(source_path, target_path)
-      {:ok, diff}
-    else
-      {:error, :invalid_version} -> {:error, :invalid_versions}
-    end
-  end
-
-  @spec compute_diff!(String.t(), String.t()) :: diff
-  defp compute_diff!(source_path, target_path) do
+  @spec compute_diff(String.t(), String.t()) :: diff
+  def compute_diff(source_path, target_path) do
     {result, _exit_code} = System.cmd("diff", ["-ruN", source_path, target_path])
 
     result
--- lib/phx_diff/diffs/diffs.ex
+++ lib/phx_diff/diffs/diffs.ex
@@ -46,9 +46,15 @@ defmodule PhxDiff.Diffs do
           {:ok, diff} | {:error, :invalid_versions}
   def get_diff(%AppSpecification{} = source_spec, %AppSpecification{} = target_spec, opts \\ [])
       when is_list(opts) do
-    opts
-    |> get_config()
-    |> DiffEngine.get_diff(source_spec, target_spec)
+    config = get_config(opts)
+
+    with {:ok, source_path} <- AppRepo.fetch_app_path(config, source_spec),
+         {:ok, target_path} <- AppRepo.fetch_app_path(config, target_spec) do
+      diff = DiffEngine.compute_diff(source_path, target_path)
+      {:ok, diff}
+    else
+      {:error, :invalid_version} -> {:error, :invalid_versions}
+    end
   end
 
   defp get_config(opts) when is_list(opts) do
