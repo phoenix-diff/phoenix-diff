cff91e9d8f80eb40377c2ec961a6df0664d2016c
Aaron Renner <aaron.renner@gmail.com>
2020-08-04 22:08:35 -0600

    Add honeybadger for error monitoring (#32)

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule PhxDiff.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.1"},
       {:logster, "~> 1.0"},
+      {:honeybadger, "~> 0.12"},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
