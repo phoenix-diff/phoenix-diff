f2c37ddcd411f61600c2db1987b51f53723b78dd
Bernhard Hackl <bernhard.hackl@sportalliance.com>
2021-09-22 16:16:16 +0200

    Introduce diff for phoenix 1.6.0-rc.0

---
+++ data/sample-app/1.6.0-rc.0/test/sample_app_web/views/layout_view_test.exs
@@ -0,0 +1,8 @@
+defmodule SampleAppWeb.LayoutViewTest do
+  use SampleAppWeb.ConnCase, async: true
+
+  # When testing helpers, you may want to import Phoenix.HTML and
+  # use functions such as safe_to_string() to convert the helper
+  # result into an HTML string.
+  # import Phoenix.HTML
+end
