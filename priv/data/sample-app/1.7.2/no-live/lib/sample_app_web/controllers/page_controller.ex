334cad31948abf44db1c46bb40ffafba84404449
Aaron Renner <aaron.renner@gmail.com>
2023-03-21 22:12:06 -0600

    Add Phoenix 1.7.2 data

---
+++ priv/data/sample-app/1.7.2/no-live/lib/sample_app_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule SampleAppWeb.PageController do
+  use SampleAppWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
