86a397a0039977fa24878367e5dc6a078592cec6
Aaron Renner <aaron.renner@gmail.com>
2023-10-10 22:28:09 +0000

    WIP

--- lib/phx_diff/diffs/diff_engine.ex
+++ lib/phx_diff/diffs/diff_engine.ex
@@ -5,15 +5,45 @@ defmodule PhxDiff.Diffs.DiffEngine do
 
   @spec compute_diff(String.t(), String.t()) :: diff
   def compute_diff(source_path, target_path) do
-    {:ok, diff} = git_diff(source_path, target_path)
+    # TODO: The diff itself takes a lot of memory, but each replacement adds to it as well
 
-    diff
-    |> String.replace(~r/((?:a|b)+)#{source_path}\//, "\\1/")
-    |> String.replace(~r/((?:a|b)+)#{target_path}\//, "\\1/")
-    # This updates the renames which don't have the a/b prefix and ensures we
-    # don't put a leading slash on these paths
-    |> String.replace(~r/#{source_path}\//, "")
-    |> String.replace(~r/#{target_path}\//, "")
+    # {:ok, diff} = git_diff(source_path, target_path)
+
+    workspace_path = tmp_dir_path()
+    File.mkdir_p!(workspace_path)
+
+    file_path = Path.join(workspace_path, "#{random_string()}.diff")
+    file_stream = File.stream!(file_path)
+
+    case System.cmd("git", [
+           "-c",
+           "core.quotepath=false",
+           "-c",
+           "diff.algorithm=histogram",
+           "diff",
+           "--no-index",
+           "--no-color",
+           source_path,
+           target_path
+         ], into: file_stream) do
+           {_stream, status} when status in [0, 1] ->
+             :ok
+         end
+
+    file_stream
+    |> Enum.join()
+    |> IO.puts()
+    # |> GitDiff.stream_patch(relative_from: source_path, relative_to: target_path)
+    # |> Stream.run()
+
+    ""
+    # diff
+    # |> String.replace(~r/((?:a|b)+)#{source_path}\//, "\\1/")
+    # |> String.replace(~r/((?:a|b)+)#{target_path}\//, "\\1/")
+    # # This updates the renames which don't have the a/b prefix and ensures we
+    # # don't put a leading slash on these paths
+    # |> String.replace(~r/#{source_path}\//, "")
+    # |> String.replace(~r/#{target_path}\//, "")
   end
 
   defp git_diff(source_path, target_path) do
@@ -32,5 +62,13 @@ defmodule PhxDiff.Diffs.DiffEngine do
       {output, 1} -> {:ok, output}
       other -> {:error, other}
     end
+  end
+
+  defp random_string do
+    Base.encode16(:crypto.strong_rand_bytes(4))
+  end
+
+  defp tmp_dir_path do
+    Path.join([System.tmp_dir!(), "diff-workspace"])
   end
 end
--- lib/phx_diff_web/live/server_side_compare_live.ex
+++ lib/phx_diff_web/live/server_side_compare_live.ex
@@ -8,6 +8,16 @@ defmodule PhxDiffWeb.ServerSideCompareLive do
   end
 
   @impl true
+  def handle_params(%{"diff_specification" => diff_specification}, _uri, socket) do
+    with {:ok, diff_specification} <- PhxDiffWeb.Params.decode_diff_spec(diff_specification) do
+      # This jumps it to 1.1 mb of memory by itself
+      PhxDiff.fetch_diff(diff_specification.source, diff_specification.target)
+    end
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
     <h1>Server Side Compare</h1>
