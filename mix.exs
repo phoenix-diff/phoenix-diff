28758150dde42cfd7280abcec3ce33d0b7f0b9a7
Aaron Renner <aaron.renner@gmail.com>
2023-10-11 20:19:04 -0600

    Update to git_diff master

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule PhxDiff.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:git_diff, github: "aaronrenner/git_diff", branch: "ar-improve-typespecs"},
+      {:git_diff, github: "mononym/git_diff"},
       {:phoenix, "~> 1.7.7"},
       {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_html, "~> 3.0"},
