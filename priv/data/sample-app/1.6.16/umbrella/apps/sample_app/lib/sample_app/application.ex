5c06412e823d21c980307d88e56a12b0351de2ad
Aaron Renner <aaron.renner@gmail.com>
2024-03-26 08:02:11 -0600

    Add 1.6.16 data

---
+++ priv/data/sample-app/1.6.16/umbrella/apps/sample_app/lib/sample_app/application.ex
@@ -0,0 +1,21 @@
+defmodule SampleApp.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      SampleApp.Repo,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: SampleApp.PubSub}
+      # Start a worker by calling: SampleApp.Worker.start_link(arg)
+      # {SampleApp.Worker, arg}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: SampleApp.Supervisor)
+  end
+end
