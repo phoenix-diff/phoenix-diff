2e1a04c4b50d638523c21f3a5e2aae2e37c91019
Aaron Renner <aaron.renner@gmail.com>
2024-04-01 21:30:44 -0600

    Initial playwright tests

---
+++ playwright/tests/phoenix-diff.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('basic version switching', async ({ page }) => {
+  await page.goto('/compare/1.6.0...1.6.1');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/v1\.6\.0/);
+
+  await expect(page.getByText('Files changed (2)')).toBeVisible();
+
+  await page.getByRole('group', { name: 'Target' }).getByLabel('Version').selectOption('1.6.0');
+
+  await expect(page.getByText('There are no changes between')).toBeVisible();
+});
