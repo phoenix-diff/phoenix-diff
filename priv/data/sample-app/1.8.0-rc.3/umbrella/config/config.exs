e2498f333e95e858e9cf69d7e1f136b4af65a570
Aaron Renner <aaron.renner@gmail.com>
2025-06-08 02:51:09 +0000

    Add 1.8.0-rc.3 sample data

---
+++ priv/data/sample-app/1.8.0-rc.3/umbrella/config/config.exs
@@ -0,0 +1,71 @@
+# This file is responsible for configuring your umbrella
+# and **all applications** and their dependencies with the
+# help of the Config module.
+#
+# Note that all applications in your umbrella share the
+# same configuration and dependencies, which is why they
+# all use the same configuration file. If you want different
+# configurations or dependencies per app, it is best to
+# move said applications out of the umbrella.
+import Config
+
+# Configure Mix tasks and generators
+config :sample_app,
+  ecto_repos: [SampleApp.Repo]
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :sample_app, SampleApp.Mailer, adapter: Swoosh.Adapters.Local
+
+config :sample_app_web,
+  ecto_repos: [SampleApp.Repo],
+  generators: [context_app: :sample_app]
+
+# Configures the endpoint
+config :sample_app_web, SampleAppWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [html: SampleAppWeb.ErrorHTML, json: SampleAppWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: SampleApp.PubSub,
+  live_view: [signing_salt: "aaaaaaaa"]
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.17.11",
+  sample_app_web: [
+    args:
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../apps/sample_app_web/assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "4.0.9",
+  sample_app_web: [
+    args: ~w(
+      --input=assets/css/app.css
+      --output=priv/static/assets/css/app.css
+    ),
+    cd: Path.expand("../apps/sample_app_web", __DIR__)
+  ]
+
+# Configures Elixir's Logger
+config :logger, :default_formatter,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
