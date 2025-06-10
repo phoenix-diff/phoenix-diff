a5e6b19c221e260e9717f05057711de2e1e170b8
Aaron Renner <aaron.renner@gmail.com>
2025-06-10 03:14:24 +0000

    Bump esbuild itself

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :phx_diff,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.20.2",
+  version: "0.25.5",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
