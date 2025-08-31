cbe7d70f8e9d096a059d05b94c572703a90aec6a
Navin Peiris <navinpeiris@gmail.com>
2025-09-01 05:27:49 +0530

    Upgrade esbuild and tailwindcss versions

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :phx_diff,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.25.5",
+  version: "0.25.9",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -34,7 +34,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.0.9",
+  version: "4.1.12",
   default: [
     args: ~w(
       --input=assets/css/app.css
