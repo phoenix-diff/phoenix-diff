ac14be970cb65a8bf5c65bfe772aca6829101967
Navin Peiris <navinpeiris@gmail.com>
2026-06-11 05:28:59 +0530

    Add phoenix 1.8.8

---
+++ priv/data/sample-app/1.8.8/no-html/assets/js/app.js
@@ -0,0 +1,18 @@
+// If you want to use Phoenix channels, run `mix help phx.gen.channel`
+// to get started and then uncomment the line below.
+// import "./user_socket.js"
+
+// You can include dependencies in two ways.
+//
+// The simplest option is to put them in assets/vendor and
+// import them using relative paths:
+//
+//     import "../vendor/some-package.js"
+//
+// Alternatively, you can `npm install some-package --prefix assets` and import
+// them using a path starting with the package name:
+//
+//     import "some-package"
+//
+// If you have dependencies that try to import CSS, esbuild will generate a separate `app.css` file.
+// To load it, simply add a second `<link>` to your `root.html.heex` file.
